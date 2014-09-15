bef367a951da882c141fbc4c3b46b0c53500528f
Cameron Edwards <cameronprattedwards@gmail.com>
2014-09-15 13:45:45 -0400

    Set up testing environment

---
+++ test-main.js
@@ -0,0 +1,24 @@
+var allTestFiles = ['bower_components/chai/chai'];
+var TEST_REGEXP = /(spec|test)\.js$/i;
+
+var pathToModule = function(path) {
+  return path.replace(/^\/base\//, '').replace(/\.js$/, '');
+};
+
+Object.keys(window.__karma__.files).forEach(function(file) {
+  if (TEST_REGEXP.test(file)) {
+    // Normalize paths to RequireJS module names.
+    allTestFiles.push(pathToModule(file));
+  }
+});
+
+require.config({
+  // Karma serves files under /base, which is the basePath from your config file
+  baseUrl: '/base',
+});
+
+require(allTestFiles, function(chai) {
+  window.should = undefined;
+  window.should = chai.should();
+  window.__karma__.start();
+});
